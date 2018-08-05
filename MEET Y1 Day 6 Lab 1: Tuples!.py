7ce944b99087f79c4c8be5aa9f21c23837ac281e
rommie20-meet <rommie20@meet.mit.edu>
2018-08-05 10:35:16 -0400

    Stopping here

---
+++ MEET Y1 Day 6 Lab 1: Tuples!.py
@@ -0,0 +1,15 @@
+x,y,z = (3,4,5)
+my_tuple = (3,4,5)
+
+print(x+y+z)
+print(my_tuple+my_tuple)
+print(x*3)
+print(my_tuple*3)
+print(my_tuple[0]==x)
+print(x[0])
+my_tuple[0]=17
+print(my_tuple)
+
+x=17
+print(x)
+
